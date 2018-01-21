330f800ff6eaba12b2b287dfedbf8a0d98fb2e4f
gilge <seret@hotmail.com>
2018-01-21 12:03:47 -0500

    add config Jenkins files

---
+++ images/master/bin/container-run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+docker run \
+    -d \
+    --name jenkins-master \
+    -p 8080:8080 \
+    -p 50000:50000 \
+    -v $(which docker):/usr/bin/docker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /root/workspace \
+    --env JAVA_OPTS=-Djenkins.install.runSetupWizard=false \
+    jenkins-master &&\
+    docker logs -f jenkins-master;
